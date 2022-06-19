49e1a0aaba232decec7f3c89f84158313212ac4d
Apple-QAQ <apple_qaq@outlook.com>
2022-06-19 19:08:30 +0800

    fix: used os module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import subprocess
+import os
 import time
 import dbus
 
@@ -7,7 +7,7 @@ import dbus
 is_send = None
 
 
-def sendLyrics(text):
+def sendDbusMessage(text):
     bus = dbus.SessionBus()
     lyrics = bus.get_object('com.yoyo.Statusbar', '/Statusbar/PermissionSurveillance')
     iface = dbus.Interface(lyrics, dbus_interface='com.yoyo.Statusbar')
@@ -16,22 +16,21 @@ def sendLyrics(text):
 
 
 def get_app_and_camera():
-    try:
-        app_lst = []
-        shell_return = subprocess.check_output(["bash", "-c", "lsof /dev/video*"]).decode()
-        return_lst = shell_return.split("\n")
-        del return_lst[0]
-        tmp_old = None
-        for _i in return_lst:
-            __tmp_split = _i.split(" ")
-            if __tmp_split[0] == tmp_old:
-                ...
-            elif __tmp_split[0] != "":
-                app_lst.append(__tmp_split[0])
-                tmp_old = __tmp_split[0]
-        return app_lst
-    except subprocess.CalledProcessError:
+    app_lst = []
+    shell_return = "".join(os.popen("bash -c 'lsof /dev/video*'").readlines())
+    if shell_return == "":
         return None
+    return_lst = shell_return.split("\n")
+    del return_lst[0]
+    tmp_old = None
+    for _i in return_lst:
+        __tmp_split = _i.split(" ")
+        if __tmp_split[0] == tmp_old:
+            ...
+        elif __tmp_split[0] != "":
+            app_lst.append(__tmp_split[0])
+            tmp_old = __tmp_split[0]
+    return app_lst
 
 
 while True:
@@ -39,11 +38,13 @@ while True:
     if is_send == __tmp_get:
         ...
     elif __tmp_get is None:
-        ret = "0"
+        ret = "{U-APPLE_QAQ-U}"
         is_send = __tmp_get
-        sendLyrics(ret)
+        sendDbusMessage(ret)
+        # print(ret)
     else:
         is_send = __tmp_get
         ret = "、".join(__tmp_get)
-        sendLyrics(ret)
+        sendDbusMessage(ret)
+        # print(ret)
     time.sleep(5)
